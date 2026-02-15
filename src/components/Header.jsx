40c0c4d713b7171f527ab70b4e3bab2ac0af3794
Anton Valkovskyi <u1remited@gmail.com>
2026-02-15 16:51:29 +0200

    animation header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,26 @@
 import BtnAudio from "./BtnAudio.jsx";
+import {useGSAP} from "@gsap/react";
+import gsap from "gsap";
 
 const Header = () => {
+
+	useGSAP(() => {
+
+		gsap.from('#header a', {
+			duration: 1,
+			ease: 'expo.In',
+			xPercent: -50,
+			opacity: 0
+		})
+
+		gsap.from('#header canvas', {
+			duration: 1,
+			ease: 'expo.In',
+			xPercent: 50,
+			opacity: 0
+		})
+	})
+
 	return (
 		<header id='header'>
 			<div className="section-container">
