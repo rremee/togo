cae1ebfbd14d5f9dbc5f69aa013e858b2aea1c4c
Anton Valkovskyi <u1remited@gmail.com>
2026-03-01 10:06:30 +0200

    Mountains: Fixed bug with empty space appearing at the bottom of Ð the component

--- src/components/Mountains.jsx
+++ src/components/Mountains.jsx
@@ -12,14 +12,14 @@ const Mountains = () => {
 		layers.forEach(layer => {
 			const speed = parseFloat(layer.dataset.speed);
 
-			gsap.from(layer, {
-				yPercent: -speed,
-				ease: 'linear',
+			gsap.to(layer,{
+				yPercent: speed,
+				ease: 'none',
 				scrollTrigger: {
 					trigger: containerRef.current,
 					start: 'top bottom',
-					end: 'bottom bottom',
-					scrub: true,
+					end: 'bottom top',
+					scrub: 0.2,
 				}
 			})
 		})
@@ -28,12 +28,12 @@ const Mountains = () => {
 
 	return (
 
-		<div ref={containerRef} id='mountains' className='relative w-d h-dvh section-margin'>
-			<img data-speed='10' src="/images/mountains/mountains-back.svg" alt="Mountains" className='mountain-decor'/>
-			<img data-speed='20' src="/images/mountains/mountains-mid-back.svg" alt="Mountains" className='mountain-decor'/>
-			<img data-speed='40' src="/images/mountains/mountains-mid-front.svg" alt="Mountains" className='mountain-decor'/>
+		<div ref={containerRef} id='mountains' className='relative w-full h-dvh section-margin'>
+			<img data-speed='30' src="/images/mountains/mountains-back.svg" alt="Mountains" className='mountain-decor'/>
+			<img data-speed='40' src="/images/mountains/mountains-mid-back.svg" alt="Mountains" className='mountain-decor'/>
+			<img data-speed='50' src="/images/mountains/mountains-mid-front.svg" alt="Mountains" className='mountain-decor'/>
 			<img data-speed='60' src="/images/mountains/mountains-front.svg" alt="Mountains" className='mountain-decor'/>
-			<img data-speed='60' src="/images/mountains/forest.svg" alt="Forest" className='mountain-decor'/>
+			<img data-speed='80' src="/images/mountains/forest.svg" alt="Forest" className='mountain-decor translate-y-2'/>
 		</div>
 	);
 };
